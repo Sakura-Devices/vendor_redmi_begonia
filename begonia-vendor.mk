azuritenoah/vendor_xiaomi_begonia

begonia: Use MIUI poweroff charging animation
Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
Change-Id: I9cd8e9574dd21b2d32e9cb22eda9608bc1e7a207

begonia-vendor.mk
@@ -6,6 +6,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/begonia
 
 PRODUCT_COPY_FILES += \
+    vendor/xiaomi/begonia/proprietary/bin/chargeonlymode:$(TARGET_COPY_OUT_SYSTEM)/bin/chargeonlymode \
     vendor/xiaomi/begonia/proprietary/bin/iwpriv:$(TARGET_COPY_OUT_SYSTEM)/bin/iwpriv \
     vendor/xiaomi/begonia/proprietary/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptXHD_encoder.so \
     vendor/xiaomi/begonia/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so \

Sakura-Devices/vendor_redmi_begonia

begonia-vendor.mk
@@ -18,6 +18,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/redmi/begonia
 
 PRODUCT_COPY_FILES += \
+    vendor/redmi/begonia/proprietary/bin/chargeonlymode:$(TARGET_COPY_OUT_SYSTEM)/bin/chargeonlymode \
     vendor/redmi/begonia/proprietary/bin/vtservice:$(TARGET_COPY_OUT_SYSTEM)/bin/vtservice \
     vendor/redmi/begonia/proprietary/etc/init/init.vtservice.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.vtservice.rc \
     vendor/redmi/begonia/proprietary/lib/libem_support_jni.so:system/lib/libem_support_jni.so \